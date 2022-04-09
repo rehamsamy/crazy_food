3c4202861d6957ec794f0061100f5b2864b1b116
Dell <reham.samy2012@yahoo.com>
2022-04-09 20:59:32 +0200

    home

--- lib/screens/home_screen/components/populat_item.dart
+++ lib/screens/home_screen/components/populat_item.dart
@@ -6,20 +6,14 @@ class PopularItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return     Padding(
+    return     
+      Padding(
       padding: const EdgeInsets.all(5.0),
-      child: Column(
-        children: [
-          Container(
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(color: kBackColor1,borderRadius: BorderRadius.circular(15),
-            ),
-            child: Center(child: Image.asset('assets/icon/icon.png')),
-          ),
-          SizedBox(height: 10,),
-        ],
+      child: ClipRRect(
+        child: Image.asset('assets/images/dumy6.jpg'),
+        borderRadius: BorderRadius.circular(20),
       ),
+
     );
   }
 }
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crazy_food/helper/constants.dart';
 import 'package:crazy_food/screens/home_screen/components/category_item.dart';
 import 'package:crazy_food/screens/home_screen/components/discount_item.dart';
 import 'package:crazy_food/screens/home_screen/components/populat_item.dart';
@@ -25,97 +26,100 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          expandedHeight: 100,
-          pinned: true,
-          floating: false,
-          centerTitle: true,
-          snap: false,
-          flexibleSpace:Column(
-            children: [
-              SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CustomTextFormField(hintText: 'search'.tr, controller: searchController,
-                  keyboardType: TextInputType.text,prefixIcon: Icons.search,radius: 15,),
-              )
+    return  Container(
+      color: kPrimaryColor,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 100,
+            pinned: true,
+            floating: false,
+            centerTitle: true,
+            snap: false,
+            flexibleSpace:Column(
+              children: [
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomTextFormField(hintText: 'search'.tr, controller: searchController,
+                    keyboardType: TextInputType.text,prefixIcon: Icons.search,radius: 15,),
+                )
 
-            ],
-          ) ,
-        ),
-        SliverList(delegate: SliverChildListDelegate(
-          [
-            Container(
-              width: double.infinity,
-              height: 500,
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.7),
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(15),topLeft:Radius.circular(15) )
-              ),
-              child: Column(
-                children: [
-                  Card(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    shape:  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+              ],
+            ) ,
+          ),
+          SliverList(delegate: SliverChildListDelegate(
+            [
+              Container(
+                width: double.infinity,
+               // height: 500,
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.7),
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(15),topLeft:Radius.circular(15) )
+                ),
+                child: Column(
+                  children: [
+                    Card(
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 8,color: Colors.white,
+                       child: Column(
+                         children: [
+                          ListTile(leading: AppText('categories'.tr,fontWeight: FontWeight.bold,color: Colors.black,),trailing:
+                            AppText('see_all'.tr,color: Colors.black,),),
+                           Container(
+                             height: 150,
+                             child: ListView.builder(
+                             scrollDirection: Axis.horizontal
+                                 ,itemCount: 10,
+                             itemBuilder: (_,index)=>
+                                CategoryItem()
+                             ),
+                           )
+                         ],
+                       ),
+                      ),
+                    Container(
+                      height: 140,
+                      child: ListView.builder(
+                         scrollDirection: Axis.horizontal
+                         ,itemCount: 10,
+                          itemBuilder: (_,index)=>
+                              DiscountItem()
+                      ),
                     ),
-                    elevation: 8,color: Colors.white,
-                     child: Column(
-                       children: [
-                        ListTile(leading: AppText('categories'.tr,fontWeight: FontWeight.bold,color: Colors.black,),trailing:
+                    Card(
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 8,color: Colors.white,
+                      child: Column(
+                        children: [
+                          ListTile(leading: AppText('popular'.tr,fontWeight: FontWeight.bold,color: Colors.black,),trailing:
                           AppText('see_all'.tr,color: Colors.black,),),
-                         Container(
-                           height: 150,
-                           child: ListView.builder(
-                           scrollDirection: Axis.horizontal
-                               ,itemCount: 10,
-                           itemBuilder: (_,index)=>
-                              CategoryItem()
-                           ),
-                         )
-                       ],
-                     ),
+                          Container(
+                            height: 120,
+                            child: ListView.builder(
+                                scrollDirection: Axis.horizontal
+                                ,itemCount: 10,
+                                itemBuilder: (_,index)=>
+                                    PopularItem()
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  Container(
-                    height: 140,
-                    child: ListView.builder(
-                       scrollDirection: Axis.horizontal
-                       ,itemCount: 10,
-                        itemBuilder: (_,index)=>
-                            DiscountItem()
-                    ),
-                  ),
-                  Card(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    shape:  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    elevation: 8,color: Colors.white,
-                    child: Column(
-                      children: [
-                        ListTile(leading: AppText('popular'.tr,fontWeight: FontWeight.bold,color: Colors.black,),trailing:
-                        AppText('see_all'.tr,color: Colors.black,),),
-                        Container(
-                          height: 120,
-                          child: ListView.builder(
-                              scrollDirection: Axis.horizontal
-                              ,itemCount: 10,
-                              itemBuilder: (_,index)=>
-                                  PopularItem()
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ]
-        ))
-      ],
+                  ],
+                ),
+              )
+            ]
+          ))
+        ],
+      ),
     );
   }
 
